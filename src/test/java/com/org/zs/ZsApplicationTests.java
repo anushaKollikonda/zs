7fae5a155eddafefda92fbd2b144c74830bb52e4
anushakollikonda <anushakollikonda@gmail.com>
2023-08-16 13:20:10 +0530

    v1

---
+++ src/test/java/com/org/zs/ZsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.zs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
